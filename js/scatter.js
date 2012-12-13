6fa32d8bf5e73795cb071b9f462c0392fde35060
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2012-12-13 11:21:59 -0800

    makes scatter.js a valid jquery function

--- js/scatter.js
+++ js/scatter.js
@@ -1,55 +1,59 @@
-var line = xChart.getVis('line-dotted');
-      var scatter1DVis = {
-        enter: function (self, storage, className, data, callbacks) {
-          line.enter.apply(this, arguments);
-          // Do your custom actions here
-          circles = storage.lineContainers.selectAll('circle')
-          .data(function (d) {
-            return d.data;
-          }, function (d) {
-            return d.x;
-          });
 
-        circles.enter().append('circle')
-          .style('opacity', 0)
-          .attr('cx', storage.lineX)
-          .attr('cy', storage.lineY)
-          .attr('r', 10);
-          // .on('mouseover', callbacks.mouseover)
-          // .on('mouseout', callbacks.mouseout)
-          // .on('click', callbacks.click);
+$(function() {
+  var line = xChart.getVis('line-dotted');
 
-          storage.lineCircles = circles;
-          
+  var scatter1DVis = {
+    enter: function (self, storage, className, data, callbacks) {
+      line.enter.apply(this, arguments);
+      // Do your custom actions here
+      circles = storage.lineContainers.selectAll('circle')
+      .data(function (d) {
+        return d.data;
+      }, function (d) {
+        return d.x;
+      });
 
-        },
-        update: function (self, storage, timing) {
-          line.update.apply(this, arguments);
-          // Do your custom actions here
-          storage.lineFills.transition().duration(timing)
-            .style('opacity', 0)
-            .attr('d', storage.lineA.y0(storage.lineY));
+    circles.enter().append('circle')
+      .style('opacity', 0)
+      .attr('cx', storage.lineX)
+      .attr('cy', storage.lineY)
+      .attr('r', 10);
+      // .on('mouseover', callbacks.mouseover)
+      // .on('mouseout', callbacks.mouseout)
+      // .on('click', callbacks.click);
 
-          storage.linePaths.transition().duration(timing)
-            .style('opacity', 0)
-            .attr('d', storage.line.y(storage.lineY));
+      storage.lineCircles = circles;
+      
 
-          storage.lineCircles.transition().duration(timing)
-            .style('opacity', 1)
-            .attr('cx', storage.lineX)
-            .attr('cy', function (d) { return self.yScale(0); })
-            // .attr('cy', storage.lineY)
-            .attr('r', 10);
+    },
+    update: function (self, storage, timing) {
+      line.update.apply(this, arguments);
+      // Do your custom actions here
+      storage.lineFills.transition().duration(timing)
+        .style('opacity', 0)
+        .attr('d', storage.lineA.y0(storage.lineY));
+
+      storage.linePaths.transition().duration(timing)
+        .style('opacity', 0)
+        .attr('d', storage.line.y(storage.lineY));
+
+      storage.lineCircles.transition().duration(timing)
+        .style('opacity', 1)
+        .attr('cx', storage.lineX)
+        .attr('cy', function (d) { return self.yScale(0); })
+        // .attr('cy', storage.lineY)
+        .attr('r', 10);
 
 
-        },
-        exit: function (self, storage, timing) {
-          line.exit.apply(this, arguments);
-          // Do your custom actions here
-        },
-        destroy: function (self, storage, timing) {
-          line.destroy.apply(this, arguments);
-          // Do your custom actions here
-        }
-      };
-      xChart.setVis('scatter-1D', scatter1DVis);
+    },
+    exit: function (self, storage, timing) {
+      line.exit.apply(this, arguments);
+      // Do your custom actions here
+    },
+    destroy: function (self, storage, timing) {
+      line.destroy.apply(this, arguments);
+      // Do your custom actions here
+    }
+  };
+  xChart.setVis('scatter-1D', scatter1DVis);
+})();
