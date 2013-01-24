2b089936329251bba2fc7b91ea6ce8f8def076ed
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2013-01-24 02:26:56 -0800

    adds histogram

--- js/beChart.circle_histogram.js
+++ js/beChart.circle_histogram.js
@@ -7,8 +7,14 @@ var BEModels = BEModels || {};
 BEModels.main = BEModels.main || {};
 BEModels.circle_histogram = BEModels.circle_histogram || {};
 
+
 // register circle-histogram as plot type
 BEModels.plot_types = BEModels.plot_type || [];
 if (BEModels.plot_types.indexOf('circle_histogram') < 0) {
   BEModels.plot_types.push('circle_histogram');
 }
+
+BEModels.circle_histogram.setType = function (old_type) {
+  // awesome transition here
+
+};
